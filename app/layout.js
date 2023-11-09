e18b28c8793390a25c800ebf6f7743165567cfeb
sahosridoy <sahosmia.webdev@gmail.com>
2023-11-09 10:12:48 +0600

    first commit move to next js

--- app/layout.js
+++ app/layout.js
@@ -1,8 +1,6 @@
-import { Inter } from 'next/font/google'
 import './globals.css'
 import { Footer, Header } from '@/components'
 
-const inter = Inter({ subsets: ['latin'] })
 
 export const metadata = {
   title: 'Create Next App',
@@ -13,7 +11,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       
-      <body className={inter.className}>
+      <body>
         <Header/>
         {children}
         <Footer/>
